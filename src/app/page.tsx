2d70faea90d4354de4f6a8543ecca41cd45074de
Ricdog266 <ricdog266@gmail.com>
2026-02-16 15:00:39 -0500

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>AI VinylPlug Studio</h1>
+      <p>Persona Extractor v0.1</p>
+      <p>If you can see this deployed, you’re officially building software.</p>
+    </main>
+  );
+}
